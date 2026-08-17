1ed771e7e956b8093d1ed7fceed4ddd5812c4913
Saket7866 <161191514+Saket7866@users.noreply.github.com>
2026-08-17 14:59:44 +0530

    Solve Sqrt(x)

---
+++ AlgorithmXlr8-P011-SqrtX/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long squareRoot = (long)Math.sqrt(x);
+        System.out.println(squareRoot);
+
+        // Write your solution here.
+        // Print the square root of x, rounded down to the nearest integer.
+    }
+}
